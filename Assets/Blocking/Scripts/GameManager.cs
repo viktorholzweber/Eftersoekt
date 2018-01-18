d5600bbf950bec7ecca186d092451d53f43b79cf
viktorholzweber <32899888+viktorholzweber@users.noreply.github.com>
2018-01-18 14:19:17 +0100

    inital commit
    initial commit

---
+++ Assets/Blocking/Scripts/GameManager.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+
+enum gameStates { defaultState, startState, midgameState, solutionState, endState };
+
+public class GameManager : MonoBehaviour 
+{
+    public CharacterController m_character;
+    public string m_gameScene;
+
+    private ArrayList collectedItems = new ArrayList();
+    private gameStates currentState = gameStates.defaultState;
+
+    public static GameManager instance;
+
+    void Start()
+    {
+        showMenu();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    public void showMenu()
+    {
+        EnableCharacter(false);
+        //show menu and controls and so on
+        startGame();
+    }
+    public void startGame()
+    {
+        Debug.Log("Game starting");
+        currentState = gameStates.startState;
+        collectedItems.Clear();
+        EnableCharacter(true);
+        //dont show menu, set cam to spawn
+        m_character.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().GroundController();
+
+        currentState = gameStates.midgameState;
+        Debug.Log("Game started");
+        TextUI.instance.ShowText("Let's see if there is something in this house...", 5);
+    }
+
+    public void endGame()
+    {
+        Debug.Log("Successfully finished the game!");
+        currentState = gameStates.endState;
+        //show finished menu
+    }
+
+    public void addCollectedItem(GameObject go)
+    {
+        Debug.Log(collectedItems.Count + " items collected!");
+        if (go.name.Equals("DeleteForSolution") && currentState==gameStates.solutionState)
+        {
+            solutionGame(go);
+        }
+        else
+        {
+            //show preview
+            //EnableCharacter(false);
+            //CloseUpObjectPreview.instance.ShowPreview(go.GetInstanceID());
+
+            //add if keyobject
+            if (go.name.Contains("KeyObject"))
+            {
+                collectedItems.Add(go);
+                Debug.Log(go.name + " collected!");
+                Debug.Log(collectedItems.Count + " items collected!");
+
+                //delete object from scene
+                Destroy(go, .5f);
+            }
+
+            //if all keyobjects -> open solution
+            if (collectedItems.Count == 3)
+            {
+                TextUI.instance.ShowText("There was a sound at the end of the hallway...", 5);
+                currentState = gameStates.solutionState;
+            }
+        }
+    }
+
+    public void solutionGame(GameObject go)
+    {
+            Destroy(go, .5f);
+        
+        //licht einschalten
+        //kasten interaktion einschalten
+        //TODO: Wand entfernen
+    }
+
+    public void EnableCharacter(bool enable)
+    {
+        m_character.enabled = enable;
+        m_character.GetComponent<FirstPersonController>().enabled = enable;
+        //m_character.GetComponentInChildren<LookAtInteraction>().enabled = enable;
+        if (enable)
+            Cursor.lockState = CursorLockMode.Locked;
+        else
+            Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void OnClosePreview()
+    {
+        EnableCharacter(true);
+    }
+
+    void Awake()
+    {
+        GameManager.instance = this;
+    }
+}
---
+++ Assets/Blocking/Scripts/UI Text/TextUI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// all UI classes are part of UnityEngine.UI namespace:
+using UnityEngine.UI;
+
+/// <summary>
+/// Display some text, optionally together with sound.
+/// Use TextUI.instance.ShowText(...) whenever a text-display is needed.
+/// </summary>
+public class TextUI : MonoBehaviour
+{
+	public Text m_text;
+	public FadeAnimation m_fade;
+	public AudioSource m_audioSource;
+
+	public void ShowText (string text, float duration)
+	{
+		m_text.text = text;
+		m_fade.FadeIn(0.25f);
+
+		StartCoroutine(Close(duration));
+	}
+
+	public void ShowText(string text, AudioClip clip, float duration)
+	{
+		m_audioSource.clip = clip;
+		m_audioSource.PlayDelayed(0.25f);
+
+		ShowText(text, duration);
+	}
+
+	private IEnumerator Close (float closeAfter)
+	{
+		yield return new WaitForSeconds(closeAfter);
+
+		m_fade.FadeOut(0.25f);
+	}
+
+	#region Accessing the object
+
+	// Kind of like a Singleton. Easy way to access MonoBehaviors that only exist once in a game. 
+	public static TextUI instance;
+
+	// Awake() is like Start() but will be called earlier. See https://docs.unity3d.com/Manual/ExecutionOrder.html
+	void Awake()
+	{
+		TextUI.instance = this;
+	}
+
+	#endregion
+}
