06c406bbba23af8838d4f098b90aa62c28024817
viktorholzweber <32899888+viktorholzweber@users.noreply.github.com>
2018-01-19 12:38:53 +0100

    added assets and text

--- Assets/Blocking/Scripts/GameManager.cs
+++ Assets/Blocking/Scripts/GameManager.cs
@@ -32,7 +32,7 @@ public class GameManager : MonoBehaviour
     public void showMenu()
     {
         EnableCharacter(false);
-        //show menu and controls and so on
+        TextUI.instance.ShowText("Let's see if there is something in this house...", 4);
         startGame();
     }
     public void startGame()
@@ -46,7 +46,7 @@ public class GameManager : MonoBehaviour
 
         currentState = gameStates.midgameState;
         Debug.Log("Game started");
-        TextUI.instance.ShowText("Let's see if there is something in this house...", 5);
+        
     }
 
     public void endGame()
@@ -76,14 +76,77 @@ public class GameManager : MonoBehaviour
                 Debug.Log(go.name + " collected!");
                 Debug.Log(collectedItems.Count + " items collected!");
 
+                //Text for KeyObjects
+                if (go.name.Equals("KeyObject1"))
+                {
+                    TextUI.instance.ShowText("I remember this object, lets take it with me...", 4);
+                }
+                if (go.name.Equals("KeyObject2"))
+                {
+                    TextUI.instance.ShowText("This is an object from one of the missing children, maybe I will need it later...", 4);
+                }
+                if (go.name.Equals("KeyObject3"))
+                {
+                    TextUI.instance.ShowText("I've seen this thing before, but can't remember where - I will take it with me...", 4);
+                }
+
                 //delete object from scene
                 Destroy(go, .5f);
+            }
+            if (go.name.Contains("UselessItem"))
+            {
+                //Text for useless objects
+
+                if (go.name.Equals("UselessItem1"))
+                {
+                    TextUI.instance.ShowText("A dirty piece of paper...", 3);
+                }
+                if (go.name.Equals("UselessItem2"))
+                {
+                    TextUI.instance.ShowText("Hmm... a torn-out page from a book... ", 3);
+                }
+                if (go.name.Equals("UselessItem3"))
+                {
+                    TextUI.instance.ShowText("A piece of paper, nothing important...", 3);
+                }
+                if (go.name.Equals("UselessItem4"))
+                {
+                    TextUI.instance.ShowText("Looks like a note...", 3);
+                }
+                if (go.name.Equals("UselessItem5"))
+                {
+                    TextUI.instance.ShowText("An empty bottle of beer...", 3);
+                }
+                if (go.name.Equals("UselessItem5"))
+                {
+                    TextUI.instance.ShowText("Seems like the home of an alcoholic... ", 3);
+                }
+                if (go.name.Equals("UselessItem6"))
+                {
+                    TextUI.instance.ShowText("Seems like the home of an alcoholic... ", 3);
+                }
+                if (go.name.Equals("UselessItem7"))
+                {
+                    TextUI.instance.ShowText("Some pills, better not try them... ", 3);
+                }
+                if (go.name.Equals("UselessItem8"))
+                {
+                    TextUI.instance.ShowText("Strange looking plastic can... ", 3);
+                }
+                if (go.name.Equals("UselessItem9"))
+                {
+                    TextUI.instance.ShowText("A brandnew book... ", 3);
+                }
+                if (go.name.Equals("UselessItem10"))
+                {
+                    TextUI.instance.ShowText("'The Lord of the Rings', I like this book... ", 3);
+                }
             }
 
             //if all keyobjects -> open solution
             if (collectedItems.Count == 3)
             {
-                TextUI.instance.ShowText("There was a sound at the end of the hallway...", 5);
+                TextUI.instance.ShowText("There was a sound at the end of the hallway...", 4);
                 currentState = gameStates.solutionState;
             }
         }
